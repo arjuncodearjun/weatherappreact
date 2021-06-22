9ac3a179fb5a3e3d0cd4b0dd5678666d8c75377c
unknown <ag.arjungupta98@gmail.com>
2021-06-22 19:31:56 +0530

    second commit

--- src/App.js
+++ src/App.js
@@ -39,7 +39,7 @@ function App() {
           <input 
             type="text"
             className="search-bar"
-            placeholder="Search..."
+            placeholder="Enter your City..."
             onChange={e => setQuery(e.target.value)}
             value={query}
             onKeyPress={search}
